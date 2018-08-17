beebb7965f19e0c77df0b3f0381e472ae4583cf6
Evgeny Golyshev <eugulixes@gmail.com>
2018-08-17 16:18:38 +0300

    Limit number of pugs

--- src/pugme.js
+++ src/pugme.js
@@ -11,6 +11,7 @@
 
 module.exports = function(robot) {
     const SERVICE_URL = "http://pugme.herokuapp.com";
+    const PUGS_LIMIT = process.env.PUGS_LIMIT || 5;
 
     robot.respond(/pug me/i, msg =>
         msg.http(`${SERVICE_URL}/random`)
@@ -19,6 +20,11 @@ module.exports = function(robot) {
 
     robot.respond(/pug bomb( (\d+))?/i, function(msg) {
         const count = msg.match[2] || 5;
+
+        if (count > PUGS_LIMIT) {
+            msg.send(`The maximum number of pugs is limited to ${PUGS_LIMIT}`);
+            return;
+        }
 
         return msg.http(`${SERVICE_URL}/bomb?count=${count}`)
             .get()((err, res, body) => Array.from(JSON.parse(body).pugs).map((pug) => msg.send(pug)));
