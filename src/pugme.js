fbca1898317fabb884fc6d914e92a2d4f473e1b9
Anton <antonio.maksimovich@gmail.com>
2018-10-09 11:53:14 +0300

    Meet eslint requirements

--- src/pugme.js
+++ src/pugme.js
@@ -9,38 +9,37 @@
 //   hubot pug bomb N - gets N pugs
 //
 
-module.exports = function(robot) {
-    const nFetch = require('node-fetch');
+module.exports = function (robot) {
+  const nFetch = require('node-fetch')
 
-    const SERVICE_URL = "http://pugme.herokuapp.com";
-    const PUGS_LIMIT = parseInt(process.env.PUGS_LIMIT, 10) || 5;
+  const SERVICE_URL = 'http://pugme.herokuapp.com'
+  const PUGS_LIMIT = parseInt(process.env.PUGS_LIMIT, 10) || 5
 
-    robot.respond(/pug me/i, async (msg) => {
-        await nFetch(`${SERVICE_URL}/random`)
-            .then(res => res.json())
-            .then(json => msg.send(json.pug))
-            .catch(err => robot.logger.error(`Failed to request a pug: ${err}`));
-    });
+  robot.respond(/pug me/i, async (msg) => {
+    await nFetch(`${SERVICE_URL}/random`)
+      .then(res => res.json())
+      .then(json => msg.send(json.pug))
+      .catch(err => robot.logger.error(`Failed to request a pug: ${err}`))
+  })
 
-    robot.respond(/pug bomb( (\d+))?/i, async (msg) => {
-        const count = msg.match[2] || 5;
+  robot.respond(/pug bomb( (\d+))?/i, async (msg) => {
+    const count = msg.match[2] || 5
 
-        if (count > PUGS_LIMIT) {
-            msg.send(`The maximum number of pugs is limited to ${PUGS_LIMIT}`);
-            return;
-        }
+    if (count > PUGS_LIMIT) {
+      msg.send(`The maximum number of pugs is limited to ${PUGS_LIMIT}`)
+      return
+    }
 
-        await nFetch(`${SERVICE_URL}/bomb?count=${count}`)
-            .then(res => res.json())
-            .then(json => Array.from(json.pugs).map((pug) => msg.send(pug)))
-            .catch(err => robot.logger.error(`Failed to request ${count} pugs: ${err}`));
-    });
+    await nFetch(`${SERVICE_URL}/bomb?count=${count}`)
+      .then(res => res.json())
+      .then(json => Array.from(json.pugs).map((pug) => msg.send(pug)))
+      .catch(err => robot.logger.error(`Failed to request ${count} pugs: ${err}`))
+  })
 
-    return robot.respond(/how many pugs are there/i, async (msg) => {
-        await nFetch(`${SERVICE_URL}/count`)
-            .then(res => res.json())
-            .then(json => msg.send(`There are ${json.pug_count} pugs.`))
-            .catch(err => robot.logger.error(`Failed to request the number of pugs: ${err}`));
-    });
-};
-
+  return robot.respond(/how many pugs are there/i, async (msg) => {
+    await nFetch(`${SERVICE_URL}/count`)
+      .then(res => res.json())
+      .then(json => msg.send(`There are ${json.pug_count} pugs.`))
+      .catch(err => robot.logger.error(`Failed to request the number of pugs: ${err}`))
+  })
+}
