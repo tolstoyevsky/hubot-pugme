4837ace6093cedfa6895b761c1ee1c83d5c72f91
Anton <antonio.maksimovich@gmail.com>
2018-10-10 15:11:17 +0300

    Add 'cat me' and 'cat bomb' commands

--- src/pugme.js
+++ src/pugme.js
@@ -13,6 +13,7 @@ module.exports = function (robot) {
   const nFetch = require('node-fetch')
 
   const SERVICE_URL = 'http://pugme.herokuapp.com'
+  const THE_CAT_API = 'https://api.thecatapi.com'
   const PUGS_LIMIT = parseInt(process.env.PUGS_LIMIT, 10) || 5
 
   robot.respond(/pug me/i, async (msg) => {
@@ -21,6 +22,13 @@ module.exports = function (robot) {
       .then(json => msg.send(json.pug))
       .catch(err => robot.logger.error(`Failed to request a pug: ${err}`))
   })
+
+  robot.respond(/cat me/i, async (msg) => {
+    await nFetch(`${THE_CAT_API}/v1/images/search`)
+        .then(res => res.json())
+        .then(json => msg.send(json[0].url))
+        .catch(err => robot.logger.error(`Failed to request a cat: ${err}`))
+	})
 
   robot.respond(/pug bomb( (\d+))?/i, async (msg) => {
     const count = msg.match[2] || 5
@@ -34,7 +42,21 @@ module.exports = function (robot) {
       .then(res => res.json())
       .then(json => Array.from(json.pugs).map((pug) => msg.send(pug)))
       .catch(err => robot.logger.error(`Failed to request ${count} pugs: ${err}`))
-  })
+	})
+
+	robot.respond(/cat bomb( (\d+))?/i, async (msg) => {
+    const count = msg.match[2] || 5
+
+    if (count > PUGS_LIMIT) { // TODO: Change PUGS_LIMIT to CUTIES_LIMIT
+      msg.send(`The maximum number of cats is limited to ${PUGS_LIMIT}`)
+      return
+    }
+
+    await nFetch(`${THE_CAT_API}/v1/images/search?limit=${count}`)
+      .then(res => res.json())
+      .then(json => Array.from(json).map((cat) => msg.send(cat.url)))
+      .catch(err => robot.logger.error(`Failed to request ${count} cats: ${err}`))
+	})
 
   return robot.respond(/how many pugs are there/i, async (msg) => {
     await nFetch(`${SERVICE_URL}/count`)
