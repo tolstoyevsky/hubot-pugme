72b7155733e2f4adc8ef4a67ff10f71624399e80
Anton <antonio.maksimovich@gmail.com>
2018-10-12 18:13:11 +0300

    Unification project for cuteme

--- src/cuteme.js
+++ src/cuteme.js
@@ -1,5 +1,5 @@
 // Description:
-//   Pugme is the most important thing in life
+//   Cuteme is the most important thing in life
 //
 // Configuration:
 //   None
@@ -12,12 +12,12 @@
 module.exports = function (robot) {
   const nFetch = require('node-fetch')
 
-  const SERVICE_URL = 'https://dog.ceo/api'
+  const PUGME_SERVICE = 'https://dog.ceo/api'
 
-  const PUGS_LIMIT = parseInt(process.env.PUGS_LIMIT, 10) || 5
+  const CUTIES_LIMIT = parseInt(process.env.CUTIES_LIMIT, 10) || 5
 
   robot.respond(/pug me/i, async (msg) => {
-    await nFetch(`${SERVICE_URL}/breed/pug/images/random`)
+    await nFetch(`${PUGME_SERVICE}/breed/pug/images/random`)
       .then(res => res.json())
       .then(json => msg.send(json.message))
       .catch(err => robot.logger.error(`Failed to request a pug: ${err}`))
@@ -26,12 +26,12 @@ module.exports = function (robot) {
   robot.respond(/pug bomb( (\d+))?/i, async (msg) => {
     const count = msg.match[2] || 5
 
-    if (count > PUGS_LIMIT) {
-      msg.send(`The maximum number of pugs is limited to ${PUGS_LIMIT}`)
+    if (count > CUTIES_LIMIT) {
+      msg.send(`The maximum number of pugs is limited to ${CUTIES_LIMIT}`)
       return
     }
 
-    await nFetch(`${SERVICE_URL}/breed/pug/images/random/${count}`)
+    await nFetch(`${PUGME_SERVICE}/breed/pug/images/random/${count}`)
       .then(res => res.json())
       .then(json => Array.from(json.message).map((pug) => msg.send(pug)))
       .catch(err => robot.logger.error(`Failed to request ${count} pugs: ${err}`))
